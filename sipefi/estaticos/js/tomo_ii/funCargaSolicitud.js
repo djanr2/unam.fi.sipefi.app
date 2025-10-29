d1338b0f8da844968b2a3ce049942edfb5a6082d
djanr2 <djanr2@gmail.com>
2025-10-28 23:59:06 -0600

    20252810: Se agrega etiqueta html hidden para ocultar funcionalidad en el front-end

--- sipefi/estaticos/js/tomo_ii/funCargaSolicitud.js
+++ sipefi/estaticos/js/tomo_ii/funCargaSolicitud.js
@@ -241,7 +241,7 @@ const fcs = function(){
 			<button class="btn btn-sm btn-danger btnEliminarRelacion">
 			  <i class="fas fa-trash-alt"></i>
 			</button>
-			<button class="btn btn-sm btn-danger" onclick="etii.descargaPdf(${idPerfil}, ${idLic}, ${idSolicitud})" >
+			<button class="btn btn-sm btn-danger" onclick="etii.descargaPdf(${idPerfil}, ${idLic}, ${idSolicitud})" hidden>
 			  <i class="fas fa-file-pdf"></i>
 			</button>
 		</div>
